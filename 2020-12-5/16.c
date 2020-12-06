aa7c2171bd1b30eccf1f214f68b7cc631ca965b2
姚存浩 <997106826@qq.com>
2020-12-06 11:16:22 +0800

    修改

--- 2020-12-5/16.c
+++ 2020-12-5/16.c
@@ -1,66 +1,60 @@
-//解法1：
-//void rotate(int* nums, int numsSize, int k){
-//	for (int i = 0; i<k; i++)
-//	{
-//		int previous = nums[numsSize - 1];
-//		for (int j = 0; j<numsSize; j++)
-//		{
-//			int temp = nums[j];
-//			nums[j] = previous;
-//			previous = temp;
-//		}
-//	}
-//
-//}
+解法1：
+void rotate(int* nums, int numsSize, int k){
+	for (int i = 0; i<k; i++)
+	{
+		int previous = nums[numsSize - 1];
+		for (int j = 0; j<numsSize; j++)
+		{
+			int temp = nums[j];
+			nums[j] = previous;
+			previous = temp;
+		}
+	}
+
+}
 #include <stdio.h>
 #include <windows.h>
 
-//解法2：void Reverse(int arr[], int left, int right)
-//{
-//	while (left<right)
-//	{
-//		int temp = arr[left];
-//		arr[left] = arr[right];
-//		arr[right] = temp;
-//		left++;
-//		right--;
-//	}
-//}
-//
-//void rotate(int* nums, int numsSize, int k){
-//	while(k > numsSize)
-//	{
-//		k -= numsSize;//多余的即为一个循环回到原点
-//	}
-//	Reverse(nums, 0, numsSize - 1);//完成整体反转
-//	Reverse(nums, 0, k - 1);//完成前半部分反转
-//	Reverse(nums, k, numsSize - 1);//完成后半部分反转
-//}
+解法2：void Reverse(int arr[], int left, int right)
+{
+	while (left<right)
+	{
+		int temp = arr[left];
+		arr[left] = arr[right];
+		arr[right] = temp;
+		left++;
+		right--;
+	}
+}
 
-//void rotate(int* nums, int numsSize, int k)
-//{
-//	while (k > numsSize)
-//	{
-//		k -=numsSize;
-//	}
-//	int *arr = (int *)malloc(sizeof(int)*numsSize);
-//	int count = 0;
-//	for (int i=numsSize-k; i < numsSize; i++)
-//	{
-//		arr[count] = nums[i];//将需要颠倒的先拷贝过去
-//		count++;
-//	}
-//	for (int j = 0; j< numsSize - k; j++)
-//	{
-//		arr[count] = nums[j];//再将后面的拷贝过来
-//		count++;
-//	}
-//	for (int j = 0; j < numsSize; j++)
-//
-//	{
-//		nums[j] = arr[j];//拷贝回原数组
-//	}
-//}
+void rotate(int* nums, int numsSize, int k){
+	k %= numsSize;
+	Reverse(nums, 0, numsSize - 1);//完成整体反转
+	Reverse(nums, 0, k - 1);//完成前半部分反转
+	Reverse(nums, k, numsSize - 1);//完成后半部分反转
+}
+
+void rotate(int* nums, int numsSize, int k)
+{
+	k %= numsSize;
+	int *arr = (int *)malloc(sizeof(int)*numsSize);
+	int count = 0;
+	for (int i=numsSize-k; i < numsSize; i++)
+	{
+		arr[count] = nums[i];//将需要颠倒的先拷贝过去
+		count++;
+	}
+	for (int j = 0; j< numsSize - k; j++)
+	{
+		arr[count] = nums[j];//再将后面的拷贝过来
+		count++;
+	}
+	for (int j = 0; j < numsSize; j++)
+
+	{
+		nums[j] = arr[j];//拷贝回原数组
+	}
+}
 void rotate(int* nums, int numsSize, int k){
 	k %= numsSize;
 	int count = 0;
