edace8c37e4732c1f826e75c26645f88f7d8accf
姚存浩 <997106826@qq.com>
2021-01-25 23:48:48 +0800

    字母异位词

--- 2021-1-23/isAnagram.c
+++ 2021-1-23/isAnagram.c
@@ -1,34 +1,23 @@
-#include <stdio.h>
-#include <windows.h>
+给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
 
-int  isAnagram(char s[], char t[]){
+bool isAnagram(char * s, char * t){
 
-	while (*s&&*t)//前面的都不相同
+	int len_s = strlen(s);
+	int len_t = strlen(t);
+	if (len_s != len_t)//长度不想等
+		return false;
+
+	int str[26] = { 0 };//26个不同的字母
+
+	for (int i = 0; i<len_t; i++)
 	{
-		if (((*s)++) == ((*t)++))
-			break;;
+		str[s[i] - 'a']++;//如果为s[i]==a，下标则为0，以此类推
+		str[t[i] - 'a']--;
 	}
-
-	while (*s || *t)//后面的都相同
+	for (int i = 0; i<26; i++)//如果对应的str中有不为0的元素，则不是字母异位词
 	{
-		if (((*s)++) != ((*t)++))
-			return 0;
+		if (str[i] != 0)
+			return false;
 	}
-	return 1;
-}
-int main()
-{
-
-	char s[] = "anagram";
-	char t[] = "nagaram";
-
-
-
-	if (isAnagram(s, t))
-		printf("有效\n");
-	else
-		printf("无效\n");
-
-	system("pause");
-	return 0;
+	return true;
 }
